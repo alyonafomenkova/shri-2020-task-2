9ba5e1dd96f71fd2379f91e4a358e053918cc6ad
Alena Fomenkova <fruitella30sep@mail.ru>
2020-01-15 15:48:50 +0300

    Updating tests for placeholder in warning block.

--- source/tests/warningTest.js
+++ source/tests/warningTest.js
@@ -2,77 +2,86 @@ const { expect } = require("chai");
 const lint = require("../linter.js");
 const reset = require("../linter.js");
 
-const validTextSizesString = `{
-    "block": "warning",
-    "content": [
-        {
-            "block": "placeholder",
-            "mods": { "size": "m" }
-        },
-        {
-            "elem": "content",
-            "content": [
-                {
-                    "block": "text",
-                    "mods": { "size": "m" }
-                },
-                {
-                    "block": "text",
-                    "mods": { "size": "m" }
-                }
-            ]
-        }
-    ]
-}`;
-
-const invalidTextSizesString = `{
-    "block": "warning",
-    "content": [
-        {
-            "block": "placeholder",
-            "mods": { "size": "m" }
-        },
-        {
-            "elem": "content",
-            "content": [
-                {
-                    "block": "text",
-                    "mods": { "size": "m" }
-                },
-                {
-                    "block": "text",
-                    "mods": { "size": "l" }
-                }
-            ]
-        }
-    ]
-}`;
-
-const validPlaceholderSize = `{
-    "block": "warning",
-    "content": [
-        { "block": "placeholder", "mods": { "size": "s" } },
-        { "block": "button", "mods": { "size": "m" } }
-    ]
-}`;
-
-const invalidPlaceholderSize = `{
-    "block": "warning",
-    "content": [
-        { "block": "placeholder", "mods": { "size": "xs" } },
-        { "block": "button", "mods": { "size": "m" } }
-    ]
-}`;
-
 describe("<<<<<     CHECKING WARNING     >>>>>", () => {
   describe("       Checking text sizes  ", () => {
-    it("TEST_1. All texts shoud be equal size.", () => {
+    it("TEST_1. All texts have equal size.", () => {
+      const validTextSizesString = `{
+        "block": "warning",
+        "content": [
+          {
+            "block": "placeholder",
+            "mods": { "size": "m" }
+          },
+          {
+            "elem": "content",
+            "content": [
+                {
+                "block": "text",
+                "mods": { "size": "m" }
+                },
+                {
+                "block": "text",
+                "mods": { "size": "m" }
+                }
+            ]
+          }
+        ]
+      }`;
       globalThis.reset();
       expect(globalThis.lint(validTextSizesString)).to.have.lengthOf(0);
     });
 
-    it("TEST_2. All texts shoud be equal size.", () => {
+    it("TEST_2. All texts have equal size, but not in warning block", () => {
+      const validTextSizesString = `{
+        "block": "test",
+        "content": [
+          {
+            "block": "placeholder",
+            "mods": { "size": "m" }
+          },
+          {
+            "elem": "content",
+            "content": [
+                {
+                "block": "text",
+                "mods": { "size": "m" }
+                },
+                {
+                "block": "text",
+                "mods": { "size": "m" }
+                }
+            ]
+          }
+        ]
+      }`;
       globalThis.reset();
+      expect(globalThis.lint(validTextSizesString)).to.have.lengthOf(0);
+    });
+
+    it("TEST_3. All texts have different sizes at one level of nesting.", () => {
+      globalThis.reset();
+      const invalidTextSizesString = `{
+        "block": "warning",
+        "content": [
+          {
+            "block": "placeholder",
+            "mods": { "size": "m" }
+          },
+          {
+            "elem": "content",
+            "content": [
+                {
+                "block": "text",
+                "mods": { "size": "m" }
+                },
+                {
+                "block": "text",
+                "mods": { "size": "l" }
+                }
+            ]
+          }
+        ]
+      }`;
       const errors = globalThis.lint(invalidTextSizesString);
 
       console.log("########################################");//
@@ -82,38 +91,116 @@ describe("<<<<<     CHECKING WARNING     >>>>>", () => {
       console.log("########################################");//
 
       expect(errors).to.have.lengthOf(1);
-      expect(errors[0]).to.have.property("code", "WARNING.TEXT_SIZES_SHOULD_BE_EQUAL");
-      expect(errors[0]).to.have.property("error", "Text sizes inside \'warning\' block are not equal");
-      expect(errors[0]).to.nested.include({"location.start.column": 1});
-      expect(errors[0]).to.nested.include({"location.start.line": 1});
-      expect(errors[0]).to.nested.include({"location.end.column": 2});
-      expect(errors[0]).to.nested.include({"location.end.line": 22});
+      expect(errors[0]).to.deep.equal({
+        code: "WARNING.TEXT_SIZES_SHOULD_BE_EQUAL",
+        error: "Text sizes inside \'warning\' block are not equal",
+        location: { start: { column: 1, line: 1 }, end: { column: 8, line: 22 } }
+      });
+    });
+
+    it("TEST_4. All texts have different sizes at different level of nesting.", () => {
+      globalThis.reset();
+      const invalidTextSizesString = `{
+        "block": "warning",
+        "content": [
+          {
+            "elem": "content",
+            "content": [
+              { "block": "text", "mods": { "size": "m" } },
+              { "block": "test", "content": { "block": "text", "mods": { "size": "l" } }}
+            ]
+          }
+        ]
+      }`;
+      const errors = globalThis.lint(invalidTextSizesString);
+
+      console.log("########################################");//
+      console.log('errors: ', errors);//
+      console.log(errors[0].location.start);//
+      console.log(errors[0].location.end);//
+      console.log("########################################");//
+
+      expect(errors).to.have.lengthOf(1);
+      expect(errors[0]).to.deep.equal({
+        code: "WARNING.TEXT_SIZES_SHOULD_BE_EQUAL",
+        error: "Text sizes inside \'warning\' block are not equal",
+        location: { start: { column: 1, line: 1 }, end: { column: 8, line: 12 } }
+      });
     });
   });
 
-  describe("       Checking placeholder size  ", () => {
-    it("TEST_1. Placeholder sizes inside warning shoud be \'s\', \'m\' or \'l\'", () => {
+  describe("    Checking placeholder sizes  ", () => {
+    it("TEST_5. Placeholder has valid size: s.", () => {
+      const validPlaceholderSize = `{
+        "block": "warning",
+        "content": [
+          { "block": "placeholder", "mods": { "size": "s" } },
+          { "block": "button", "mods": { "size": "m" } }
+        ]
+      }`;
       globalThis.reset();
       expect(globalThis.lint(validPlaceholderSize)).to.have.lengthOf(0);
     });
 
-    it("TEST_1. Placeholder sizes inside warning shoud be \'s\', \'m\' or \'l\'", () => {
+    it("TEST_6. Placeholder has valid size: m.", () => {
+      const validPlaceholderSize = `{
+        "block": "warning",
+        "content": [
+          { "block": "placeholder", "mods": { "size": "m" } },
+          { "block": "button", "mods": { "size": "m" } }
+        ]
+      }`;
       globalThis.reset();
+      expect(globalThis.lint(validPlaceholderSize)).to.have.lengthOf(0);
+    });
+
+    it("TEST_7. Placeholder has valid size: l.", () => {
+      const validPlaceholderSize = `{
+        "block": "warning",
+        "content": [
+          { "block": "placeholder", "mods": { "size": "l" } },
+          { "block": "button", "mods": { "size": "m" } }
+        ]
+      }`;
+      globalThis.reset();
+      expect(globalThis.lint(validPlaceholderSize)).to.have.lengthOf(0);
+    });
+
+    it("TEST_8. Placeholder has valid size: l, but not in warning block.", () => {
+      const validPlaceholderSize = `{
+        "block": "test",
+        "content": [
+          { "block": "placeholder", "mods": { "size": "l" } },
+          { "block": "button", "mods": { "size": "m" } }
+        ]
+      }`;
+      globalThis.reset();
+      expect(globalThis.lint(validPlaceholderSize)).to.have.lengthOf(0);
+    });
+
+    it("TEST_9. Placeholder has invalid size: xs.", () => {
+      globalThis.reset();
+      const invalidPlaceholderSize = `{
+        "block": "warning",
+        "content": [
+          { "block": "placeholder", "mods": { "size": "xs" } },
+          { "block": "button", "mods": { "size": "m" } }
+        ]
+      }`;
       const errors = globalThis.lint(invalidPlaceholderSize);
 
-      console.log("########################################");
-      console.log('ERRORS!!!!!!!!!!!!!!!!!!!!!!!!!!!: ', errors);//
-      console.log(errors[0].location.start);
-      console.log(errors[0].location.end);
-      console.log("########################################");
+      console.log("########################################");//
+      console.log('errors: ', errors);//
+      console.log(errors[0].location.start);//
+      console.log(errors[0].location.end);//
+      console.log("########################################");//
 
       expect(errors).to.have.lengthOf(1);
-      expect(errors[0]).to.have.property("code", "WARNING.INVALID_PLACEHOLDER_SIZE");
-      expect(errors[0]).to.have.property("error", "Placeholder sizes inside 'warning' shoud be \"s\", \"m\", or \"l\"");
-      expect(errors[0]).to.nested.include({"location.start.column": 9});
-      expect(errors[0]).to.nested.include({"location.start.line": 4});
-      expect(errors[0]).to.nested.include({"location.end.column": 61});
-      expect(errors[0]).to.nested.include({"location.end.line": 4});
+      expect(errors[0]).to.deep.equal({
+        code: "WARNING.INVALID_PLACEHOLDER_SIZE",
+        error: "Placeholder sizes inside 'warning' shoud be \"s\", \"m\", or \"l\"",
+        location: { start: { column: 11, line: 4 }, end: { column: 63, line: 4 } }
+      });
     });
   });
 });
